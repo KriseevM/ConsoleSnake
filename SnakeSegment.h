9747dd607d391fe28d61ea320625e296598c114e
KriseevM <mish.kryseev@gmail.com>
2022-05-24 12:31:50 +0300

    Removed description comment in SnakeSegment it wasn't relevant anymore

--- SnakeSegment.h
+++ SnakeSegment.h
@@ -5,21 +5,6 @@
 #ifndef CONSOLESNAKE_SNAKESEGMENT_H
 #define CONSOLESNAKE_SNAKESEGMENT_H
 
-// Segments are organized in kind of linked list.
-// There's a first one - head and each segment has a pointer or reference to the next one.
-// The last one is a tail which just contains nullptr instead of another segment.
-//
-// Fields:
-// x position : int (has a getter)
-// y position : int (has a getter)
-// follower : SnakeSegment*
-// Methods:
-// Draw - draw a segment
-// Hide - draw a space instead of a segment
-// DrawAll - calls Draw on a segment and on each segment after it
-// HideAll - calls Hide on a segment and on each segment after it
-// MoveTo (x : int, y : int) - moves a segment to specified position, and then calls recursively Move on follower
-
 #include <ncurses.h>
 
 class SnakeSegment {
