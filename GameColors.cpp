1715c6db4b36616941f2858a6d2e0034c7ebf9d8
KriseevM <mish.kryseev@gmail.com>
2022-05-24 11:07:00 +0300

    Changed colors a bit

--- GameColors.cpp
+++ GameColors.cpp
@@ -7,6 +7,6 @@ void GameColors::initialize()
     init_pair(GameColors::MenuSelectedPair, COLOR_WHITE, COLOR_BLUE);
     init_pair(GameColors::MenuUnselectedPair, COLOR_WHITE, COLOR_BLACK);
     init_pair(GameColors::GameFieldPair, COLOR_WHITE, COLOR_GREEN);
-    init_pair(GameColors::SnakePair, COLOR_MAGENTA, COLOR_GREEN);
-    init_pair(GameColors::FruitPair, COLOR_CYAN, COLOR_GREEN);
+    init_pair(GameColors::SnakePair, COLOR_WHITE, COLOR_GREEN);
+    init_pair(GameColors::FruitPair, COLOR_RED, COLOR_GREEN);
 }
